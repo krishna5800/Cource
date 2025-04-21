68eb2237afd7a5558e3022abfd16e41396c68d32
Krrish1011H <krishnahitnalikar7@gmail.com>
2025-04-21 23:20:49 +0530

    git commit -m "Updated 6.cpp and recompiled 6.exe"

--- 6.cpp
+++ 6.cpp
@@ -1,15 +1,89 @@
+// BIT-WISE OPERATORS AND ALL 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-     int a,b;
+	// int a,b;
 
-     cin >> a;
-     cin >> b;
+	// cin >> a;
+	// cin >> b;
 
-     cout << (a & b) << endl;
+	// cout << (a&b) << endl;
+	// return 0;
 
-     return 0;
+
+	// int a,b;
+
+	// a =5;
+	// b = 10;
+
+	// cout << (a^b) << endl;
+	// return 0;
+
+
+	// a = 5;
+	// b = -5;
+
+	// cout << (a^b);
+
+
+	// LEFT AND RIGHT SHIFT OPERATORS 
+
+	// LEFT SHIFT OPERATOR === " << "
+
+	// RIGHT SHIFT OPERATOR === " >> "
+
+
+	// int a;
+	
+	// a = 2;
+
+	// cout << (a << 1) << endl;
+
+	// WHEN WE SHIFT LEFT 1 BIT OUR NUMBER / VALUE GEST DOUBLED
+
+	// cout << (a << 2) << endl;
+
+	// int a;
+
+	// a = 100;
+
+	// cout << (a >> 1) << endl;
+
+	// WHEN WE RIGHT SHIFT BY N BITS OUR NUMBER / VALUE IS DIVIDED BY 2^N
+	
+	// THIS ABOVE STSTEMENT IS NOT TRUE ALWAYS 
+
+	// EXAMPLE
+
+	// int n;
+
+	// n = 5;
+
+	// cout << (n >> 1) << endl;
+	//NOW IN THIS EXAMPLE OUR OUTPUT IS NOT ACCORDINT TOTHAT RULE
+
+	// int n;
+
+	// n = -100;
+
+	// cout << (n >> 1);
+
+	// now output is == -50
+
+
+	// unsigned int n;
+
+	// n = -100;
+
+	// cout << (n >> 1);
+
+	// now our output changes to some value == 2147483598
+
+
+
+
+	return 0;
 }
