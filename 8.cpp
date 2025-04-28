7da43bbc4f1915cc35954bcb0b5fc9bb2c4eae73
Krrish1011H <krishnahitnalikar7@gmail.com>
2025-04-28 20:09:02 +0530

    Added new C++ source file 8.cpp and executable 8.exe

---
+++ 8.cpp
@@ -0,0 +1,150 @@
+// FUNCTIONS IN C++
+
+// MAKING SIMPLE FUNCTIONS
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void sentence()
+// {
+//     for(int i = 0; i < 20; i++)
+//     {
+//         cout << "Sundar Ko Sundari Psand Hai !!" << endl;
+//     }
+// }
+
+// void sum(int a, int b)
+// {
+//     cout << a+b << endl;
+// }
+
+// int main()
+// {
+//     int a,b;
+//     cin >> a;
+//     cin >> b;
+
+//     // sum(a, b);
+//     // sentence();
+//     return 0;
+// }
+
+// MAKING SIMPLE CALCY USING FUNCTIONS
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void sum(int a, int b)
+// {
+//     cout << "Answer -";
+//     cout << a + b << endl;
+// }
+
+// void sub(int a, int b)
+// {
+//     cout << "Answer -";
+//     cout << a - b << endl;
+// }
+
+// void multiply(int a, int b)
+// {
+//     cout << "Answer -";
+//     cout << a * b << endl;
+// }
+
+// void division(int a, int b)
+// {
+//     cout << "Answer -";
+//     cout << a / b << endl;
+// }
+
+// int main()
+// {
+
+//     int a,b;
+//     cout << "Enter num1 -" << endl;
+//     cin >> a;
+//     cout << "Enter num2 -" << endl;
+//     cin >> b;
+
+//     cout << "WELCOME TO CALCY -" << endl;
+//     cout << "1 - for Addition" << endl;
+
+//     cout << "2 - for Substraction" << endl;
+
+//     cout << "3 - for Multiplication" << endl;
+
+//     cout << "4 - for Division" << endl;
+
+//     cout << "Enter Number According which operation you want to perform -" << endl;
+
+//     int c;
+//     cin >> c;
+//     switch (c)
+//     {
+//     case 1:
+//         sum(a ,b);
+//         break;
+
+//     case 2:
+//         sub(a ,b);
+//         break;
+
+//     case 3:
+//         multiply(a, b);
+//         break;
+
+//     case 4:
+//         division(a ,b);
+//         break;
+
+//     default:
+//         cout << "Invalid Input" << endl;
+//         break;
+//     }
+
+
+//     return 0;
+// }
+
+
+// FUNCTION CALL STACK
+
+
+// WHICH IS 3 NUMBERS ID GREATEST
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// int sum(int a, int b, int c)
+// {
+//     if(a > b && a > c)
+//     {
+//         cout << "Greatest number is - " << a << endl;
+//     }
+//     else if(b > c && c > a)
+//     {
+//         cout << "Greatest number is - " << c << endl;
+//     }
+//     else 
+//     {
+//         cout << "Greatest number is - " << c << endl;
+//     }
+// }
+// int main()
+// {
+//     int a,b,c;
+//     cout << "Enter 3 Numbers - " << endl;
+//     cin >> a;
+//     cin >> b;
+//     cin >> c;
+
+//     sum(a, b, c);
+
+//     return 0;
+// }
+
+
+// CLASS WORK 
+
